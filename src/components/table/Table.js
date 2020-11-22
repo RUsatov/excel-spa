6e3a862c5ed9bead24f9c0a5d01925b5d2c93b35
Roman Usatov <R.Usatov76@gmail.com>
2020-11-22 12:40:15 +0300

    Remove comments

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -6,35 +6,5 @@ export class Table extends ExcelComponent {
 
   toHtml () {
     return createTable()
-    // `
-    //   <div class="row">
-    //     <div class="row-info"></div>
-    //
-    //     <div class="row-data">
-    //       <div class="column">A</div>
-    //       <div class="column">B</div>
-    //       <div class="column">C</div>
-    //     </div>
-    //   </div>
-    //
-    //   <div class="row">
-    //     <div class="row-info">1</div>
-    //
-    //     <div class="row-data">
-    //       <div class="cell selected" contenteditable>A1</div>
-    //       <div class="cell" contenteditable>B1</div>
-    //       <div class="cell" contenteditable>C1</div>
-    //     </div>
-    //   </div>
-    //   <div class="row">
-    //     <div class="row-info">2</div>
-    //
-    //     <div class="row-data">
-    //       <div class="cell">A1</div>
-    //       <div class="cell">B1</div>
-    //       <div class="cell">C1</div>
-    //     </div>
-    //   </div>
-    // `
   }
 }
